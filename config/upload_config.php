0d7e0dd1d155a20f4e8fd80b83643b86a74f83d5
khauvannam <nkhauvan@gmail.com>
2024-11-09 10:17:07 +0700

    nothing

--- config/upload_config.php
+++ config/upload_config.php
@@ -1,37 +1,99 @@
 <?php
+// Define file upload path
+$upload_dir = array(
+    'img' => 'uploads/',
+);
 
+// Allowed image properties
+$imgset = array(
+    'maxsize' => 5000,
+    'maxwidth' => 4096,
+    'maxheight' => 3000,
+    'minwidth' => 10,
+    'minheight' => 10,
+    'type' => array('bmp', 'gif', 'jpg', 'jpeg', 'png'),
+);
 
-// Allow any origin to access this resource
-header("Access-Control-Allow-Origin: *");
+// If 0, will OVERWRITE the existing file
+const RENAME_F = 1;
 
-// Set content type to JSON for CKEditor response
-header('Content-Type: application/json');
+$site = '';
 
+/**
+ * Set filename
+ * If the file exists, and RENAME_F is 1, set "img_name_1"
+ *
+ * $p = dir-path, $fn=filename to check, $ex=extension $i=index to rename
+ */
+function setFName($p, $fn, $ex, $i): string
+{
+    if (RENAME_F == 1 && file_exists($p . $fn . $ex)) {
+        return setFName($p, F_NAME . '_' . ($i + 1), $ex, ($i + 1));
+    } else {
+        return $fn . $ex;
+    }
+}
 
-if (isset($_FILES['upload']) && is_array($_FILES['upload']['name'])) {
-    $urls = [];
-    foreach ($_FILES['upload']['name'] as $index => $originalName) {
-        if ($_FILES['upload']['error'][$index] === 0) {
-            // Folder and filename setup
-            $folderName = isset($_POST['name']) ? str_replace(' ', '_', trim($_POST['name'])) : 'default';
-            $uploadDir = 'uploads/' . $folderName . '/';
-            if (!is_dir($uploadDir)) {
-                mkdir($uploadDir, 0777, true);
+$re = '';
+if (isset($_FILES['upload']) && strlen($_FILES['upload']['name']) > 1) {
+
+    define('F_NAME', preg_replace('/\.(.+?)$/i', '', basename($_FILES['upload']['name'])));
+
+    // Get filename without extension
+    $sepext = explode('.', strtolower($_FILES['upload']['name']));
+    $type = end($sepext);
+    /** gets extension **/
+
+    // Upload directory
+    $upload_dir = in_array($type, (array)$imgset['type']) ? $upload_dir['img'] : $upload_dir['audio'];
+    $upload_dir = trim($upload_dir, '/') . '/';
+
+    // Validate file type
+    if (in_array($type, (array)$imgset['type'])) {
+        // Image width and height
+        list($width, $height) = getimagesize($_FILES['upload']['tmp_name']);
+
+        if (isset($width) && isset($height)) {
+            if ($width > $imgset['maxwidth'] || $height > $imgset['maxheight']) {
+                $re .= ' Width x Height = ' . $width . ' x ' . $height . ' >>> The maximum Width x Height must be: ' . $imgset['maxwidth'] . ' x ' . $imgset['maxheight'];
             }
-            // File with unique timestamp name
-            $extension = pathinfo($originalName, PATHINFO_EXTENSION);
-            $filename = round(microtime(true) * 1000) . '.' . $extension;
-            $targetFilePath = $uploadDir . $filename;
 
-            if (move_uploaded_file($_FILES['upload']['tmp_name'][$index], $targetFilePath)) {
-                $urls[] = ['url' => '/' . $targetFilePath];
-            } else {
-                echo json_encode(['error' => ['message' => 'Failed to move uploaded file.']]);
-                exit;
+            if ($width < $imgset['minwidth'] || $height < $imgset['minheight']) {
+                $re .= ' Width x Height = ' . $width . ' x ' . $height . ' >>> The minimum Width x Height must be: ' . $imgset['minwidth'] . ' x ' . $imgset['minheight'];
+            }
+
+            if ($_FILES['upload']['size'] > $imgset['maxsize'] * 1000) {
+                $re .= ' >>> Maximum file size must be: ' . $imgset['maxsize'] . ' KB.';
             }
         }
+    } else {
+        $re .= 'The file: ' . $_FILES['upload']['name'] . ' has not the allowed extension type.';
     }
-    echo json_encode($urls); // Return array of URLs for all files
-} else {
-    echo json_encode(['error' => ['message' => 'No files uploaded or upload error occurred.']]);
+
+    // File upload path
+    $f_name = setFName($_SERVER['DOCUMENT_ROOT'] . '/' . $upload_dir, F_NAME, ".$type", 0);
+    $uploadPath = $upload_dir . $f_name;
+
+    // If no errors, upload the image, else, output the errors
+    if ($re == '') {
+        if (move_uploaded_file($_FILES['upload']['tmp_name'], $uploadPath)) {
+            $url = $site . 'ckeditor/' . $upload_dir . $f_name;
+            $msg = F_NAME . '.' . $type . ' successfully uploaded! >>> Size: ' . number_format($_FILES['upload']['size'] / 1024, 2, '.', '') . ' KB';
+            $response = [
+                'url' => $url
+            ];
+        } else {
+            $response = [
+                'error' => [
+                    'message' => 'Unable to upload the file!'
+                ]
+            ];
+        }
+    } else {
+        $response = [
+            'error' => [
+                'message' => 'Error: ' . $re
+            ]
+        ];
+    }
 }
